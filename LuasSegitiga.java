64e40e744228bab3ff9e2afc1e7979b2fe5b22c1
Fiuxs <muftahillah6@gmail.com>
2024-09-20 17:42:35 +0800

    Add files via upload

---
+++ LuasSegitiga.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package tipedata;
+
+/**
+ *
+ * @author LENOVO
+ */
+public class LuasSegitiga {
+
+    public static void main(String[] args) {
+        double alas;
+        double tinggi;
+
+        alas = 7;
+        tinggi = 3;
+
+        double rumus = alas * tinggi / 2;
+        System.out.println("Luas segitiga : " + rumus);
+    }
+}
